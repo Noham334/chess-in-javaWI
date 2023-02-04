d9616f4923c095b80ee11737957ef914a8e396ed
Israel Hurtado <israelmateus04@gmail.com>
2023-02-04 15:02:15 -0400

    Check Logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,19 +6,26 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 
-    private Color color;
+        private Color color;
 
-    public ChessPiece(Board board, Color color) {
-        super(board);
-        this.color = color;
-    }
+        public ChessPiece(Board board, Color color) {
+            super(board);
+            this.color = color;
+        }
 
-    public Color getColor(){
-        return color;
-    }
+        public Color getColor(){
+            return color;
+        }
 
-    protected boolean isThereOpponentPiece(Position position){
-        ChessPiece p = (ChessPiece) getBoard().piece(position);
-        return p != null && p.getColor() != color;
-    }
+        public ChessPosition getChessPosition(){
+            return ChessPosition.fromPosition(position);
+        }
+
+        protected boolean isThereOpponentPiece(Position position){
+            ChessPiece p = (ChessPiece) getBoard().piece(position);
+            return p != null && p.getColor() != color;
+        }
+
+
+
 }
